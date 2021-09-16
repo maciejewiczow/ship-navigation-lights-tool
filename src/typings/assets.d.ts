12eb37cb85840849ee32aa9ccfb390cfe33dee62
Mafciejewicz <melonik0.9846@gmail.com>
2021-09-16 03:25:38 +0200

    Initial setup

---
+++ src/typings/assets.d.ts
@@ -0,0 +1,14 @@
+declare module '*.glsl' {
+    const path: string;
+    export default path;
+}
+
+declare module '*.glb' {
+    const path: string;
+    export default path;
+}
+
+declare module '*.hdr' {
+    const path: string;
+    export default path;
+}
