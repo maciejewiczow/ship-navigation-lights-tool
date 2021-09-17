d7ca061b455cf5a51a9d5a8a701d54500ce17a3f
Mafciejewicz <melonik0.9846@gmail.com>
2021-09-18 00:19:31 +0200

    Add bootstrap and icons

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createGlobalStyle } from 'styled-components';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import store from './store';
 import App from './App';
