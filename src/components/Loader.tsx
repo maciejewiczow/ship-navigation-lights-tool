35f71874ce372a996d3f17f2750ecfc6d6fd6c87
Mafciejewicz <melonik0.9846@gmail.com>
2021-09-18 00:20:56 +0200

    Add animated loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+`;
+
+const ChildrenWrapper = styled.div`
+    width: 100%;
+    color: white;
+    margin-top: 20px;
+    text-align: center;
+`;
+
+const AnimationWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    grid-gap: 1px;
+
+    width: 100px;
+    height: 100px;
+
+    /* width: 100%; */
+`;
+
+const Box = styled.div<{ delay: string }>`
+    background: transparent;
+    color:white;
+
+    animation: fade 2s infinite;
+    animation-delay: ${({ delay }) => delay};
+
+    @keyframes fade {
+        0% {
+            background: transparent;
+        }
+        25% {
+            background: white;
+        }
+        50%, 100% {
+            background: transparent;
+        }
+    }
+`;
+
+export const Loader: React.FC = ({ children }) => (
+    <Wrapper>
+        <AnimationWrapper>
+            {new Array(16).fill(0).map((_, i) => <Box delay={`${((i % 4) + Math.floor(i / 4)) * 140}ms`} />)}
+        </AnimationWrapper>
+        <ChildrenWrapper>{children ?? 'Ładowanie...'}</ChildrenWrapper>
+    </Wrapper>
+);
