abcdd3826911eac7016e6da265ba89209185690c
Mafciejewicz <melonik0.9846@gmail.com>
2021-09-19 00:21:58 +0200

    Change reduce to every

--- src/utils/objHasOwnProperty.ts
+++ src/utils/objHasOwnProperty.ts
@@ -3,5 +3,5 @@ export const objHasOwnProperty = (object: unknown, propName: string | number | s
 );
 
 export const objHasOwnProperties = (object: unknown, propNames: Array<string | number | symbol>): boolean => (
-    propNames.reduce<boolean>((result, prop) => result && objHasOwnProperty(object, prop), true)
+    propNames.every(prop => objHasOwnProperty(object, prop))
 );
