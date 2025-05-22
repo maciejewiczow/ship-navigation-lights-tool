a03150bf90cda6ea21230fd4c19ad67656e93bea
Mafciejewicz <melonik0.9846@gmail.com>
2025-05-22 13:57:41 +0200

    Add back button to controls

--- src/views/ControlsView/ControlsView.tsx
+++ src/views/ControlsView/ControlsView.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router';
 import { Controls } from '~/components/Controls';
-import { currentSceneDesc } from '~/store/Scenes/selectors';
-import { Wrapper } from './parts';
+import { currentSceneDescriptor } from '~/store/Scenes/selectors';
+import { BackIcon, BackLink, Wrapper } from './parts';
 
 export const ControlsView: React.FC = () => {
-    const desc = useSelector(currentSceneDesc);
+    const scene = useSelector(currentSceneDescriptor);
 
-    if (!desc) {
+    if (!scene) {
         return <Navigate to="/" />;
     }
 
     return (
         <Wrapper>
-            <h3>{desc.name}</h3>
+            <div>
+                <BackLink to="..">
+                    {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+                    {/* @ts-ignore */}
+                    <Button variant="light">
+                        <BackIcon />
+                        Powrót
+                    </Button>
+                </BackLink>
+            </div>
+            <h3>{scene.name}</h3>
             <Controls />
         </Wrapper>
     );
--- src/views/ControlsView/parts.ts
+++ src/views/ControlsView/parts.ts
@@ -1,7 +1,28 @@
+import { TfiAngleLeft } from 'react-icons/tfi';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
     padding: 12px 24px;
     max-width: 700px;
     margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+`;
+
+export const BackLink = styled(Link)`
+    text-decoration: none;
+
+    .btn {
+        margin-left: calc(-1 * var(--bs-btn-padding-x));
+        display: flex;
+        gap: 6px;
+        align-items: center;
+    }
+`;
+
+export const BackIcon = styled(TfiAngleLeft)`
+    margin-top: 1px;
 `;
